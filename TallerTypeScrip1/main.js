658ddc747d008c18ac5aa201e8efa33cc6b586a9
isalozanoron <123670783+isalozanoron@users.noreply.github.com>
2024-04-14 23:56:53 -0500

    Update main.js

--- TallerTypeScrip1/main.js
+++ TallerTypeScrip1/main.js
@@ -2,16 +2,16 @@ import { series } from './data.js';
 var seriesTbody = document.getElementById('series');
 var averageSeasonsCell = document.getElementById('averageSeasons');
 renderSeriesInTable(series);
-function renderSeriesInTable(series) {
-    console.log('Desplegando cursos');
-    var totalSeasons = 0;
+function renderSeriesTabla(series) {
+    var totalTemporadas = 0;
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
         var link = "<a href=\"".concat(serie.enlace, "\">").concat(serie.nombre, "</a>");
         trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                          <td>").concat(link, "</td>\n                          <td>").concat(serie.canal, "</td>\n                          <td>").concat(serie.numTemporadas, "</td>");
-        totalSeasons += serie.numTemporadas;
-        seriesTbody.appendChild(trElement);
+        totalTemporadas += serie.numTemporadas;
+        seriesTabla.appendChild(trElement);
     });
-    var averageSeasons = totalSeasons / series.length;
-    averageSeasonsCell.textContent = averageSeasons.toFixed(0);
+    var averageSeasons = totalTemporadas / series.length;
+    PromedioTemporadas.textContent = averageSeasons.toFixed(0);
+    
 }
